57df3c095d5bb191545dbc77dcf9b53964ad96b6
jaume768 <jaumefernandezsunyer10@gmail.com>
2024-11-12 22:42:30 +0100

    carruserl de fotos hecho y arreglado error de navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+// src/pages/Home.js
 import React, { useEffect, useState } from 'react';
 import api from '../services/api';
 import Category from '../components/Category';
 import './css/Home.css';
+import Slider from "react-slick"; // Importa Slider de react-slick
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
     const [categorias, setCategorias] = useState([]);
@@ -32,9 +36,35 @@ const Home = () => {
         fetchBrands();
     }, []);
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000, // 5 segundos
+        arrows: true,
+        pauseOnHover: true,
+    };
+
+    const images = [
+        'https://xcimg.szwego.com/wsxcWeb/01fdb4dd/o_1i0ladm3775fsb31urc48q6s5c.jpg',
+        'https://xcimg.szwego.com/wsxcWeb/01fdb4dd/o_1i0ladosm77l1m3g10al1vaf1dajk.jpg',
+        'https://xcimg.szwego.com/wsxcWeb/01fdb4dd/o_1i0ladrsr1o0a1v221phs9h81gr4s.jpg',
+        'https://xcimg.szwego.com/wsxcWeb/01fdb4dd/o_1i0ladv9a17171ted14fkolup8k12.jpg',
+        'https://xcimg.szwego.com/wsxcWeb/01fdb4dd/o_1i0lae23e4g512t5vir2q51s2g18.jpg',
+    ];
+
     return (
         <div className="home">
-            <img src="/assets/main.jpeg" alt="Principal" className="main-image" />
+            <Slider {...settings}>
+                {images.map((image, index) => (
+                    <div key={index}>
+                        <img src={image} alt={`Slide ${index + 1}`} className="carousel-image" />
+                    </div>
+                ))}
+            </Slider>
             {categorias.map((categoria) => {
                 const marcasDeCategoria = marcas.filter(brand => brand.categoria._id === categoria._id);
                 console.log(`Marcas para la categoría ${categoria.nombre}:`, marcasDeCategoria);
