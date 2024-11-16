ee57f2dfee2d08cdbba4dd4a45e4df44ea0066f4
Jaume Fernández <jaumefernandez@carnicassunyer.com>
2024-11-16 12:15:56 +0100

    retoques finals css

--- src/pages/BrandProducts.js
+++ src/pages/BrandProducts.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { FaDownload, FaShareAlt } from 'react-icons/fa';
 import api from '../services/api';
@@ -10,25 +10,58 @@ const BrandProducts = () => {
     const [brand, setBrand] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+    const observer = useRef();
+
+    const lastProductElementRef = useCallback(node => {
+        if (loading) return;
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) {
+                setPage(prevPage => prevPage + 1);
+            }
+        });
+        if (node) observer.current.observe(node);
+    }, [loading, hasMore]);
 
     useEffect(() => {
-        const fetchBrandAndProducts = async () => {
+        const fetchBrandAndInitialProducts = async () => {
             try {
                 const brandRes = await api.get(`/brands/${id}`);
                 setBrand(brandRes.data);
-
-                const productsRes = await api.get(`/products/brand/${id}`);
-                setProducts(productsRes.data);
             } catch (err) {
-                console.error('Error al obtener datos de la marca o productos:', err);
-                setError('No se pudieron cargar los datos.');
+                console.error('Error al obtener datos de la marca:', err);
+                setError('No se pudo cargar la marca.');
+                setLoading(false);
+            }
+        };
+
+        fetchBrandAndInitialProducts();
+    }, [id]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            setLoading(true);
+            try {
+                const res = await api.get(`/products/brand/${id}`, {
+                    params: { page, limit: 10 },
+                });
+                const fetchedProducts = res.data.products;
+                setProducts(prevProducts => [...prevProducts, ...fetchedProducts]);
+                setHasMore(page < res.data.totalPages);
+            } catch (err) {
+                console.error('Error al obtener productos:', err);
+                setError('No se pudieron cargar los productos.');
             } finally {
                 setLoading(false);
             }
         };
 
-        fetchBrandAndProducts();
-    }, [id]);
+        if (brand) {
+            fetchProducts();
+        }
+    }, [id, page, brand]);
 
     const handleShare = (productId, event) => {
         event.preventDefault();
@@ -41,16 +74,16 @@ const BrandProducts = () => {
     const handleDownload = async (imageUrl, productName, event) => {
         event.preventDefault();
         event.stopPropagation();
-    
+
         try {
             const response = await fetch(imageUrl, {
                 mode: 'cors',
             });
-    
+
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-    
+
             const blob = await response.blob();
             const url = window.URL.createObjectURL(blob);
             const link = document.createElement('a');
@@ -66,10 +99,6 @@ const BrandProducts = () => {
         }
     };
 
-    if (loading) {
-        return <div className="loading">Cargando...</div>;
-    }
-
     if (error) {
         return <div className="error-message">{error}</div>;
     }
@@ -79,53 +108,96 @@ const BrandProducts = () => {
             {brand && (
                 <div className="brand-header">
                     {brand.categoria && brand.categoria.fotoUrl && (
-                        <img 
-                            src={brand.categoria.fotoUrl} 
-                            alt={`Categoría: ${brand.categoria.nombre}`} 
-                            className="brand-header-image" 
+                        <img
+                            src={brand.categoria.fotoUrl}
+                            alt={`Categoría: ${brand.categoria.nombre}`}
+                            className="brand-header-image"
                         />
                     )}
                     <h1 className="brand-header-name">{brand.nombre}</h1>
                 </div>
             )}
             <div className="products-list">
-                {products.length > 0 ? (
-                    products.map(product => (
-                        <div key={product._id} className="product-item-container">
-                            <Link to={`/products/${product._id}`} className="product-link">
-                                <div className="product-item">
-                                    <img src={product.imagenes[0]} alt={product.nombre} className="product-image" />
+                {products.map((product, index) => {
+                    if (products.length === index + 1) {
+                        return (
+                            <div
+                                key={product._id}
+                                className="product-item-container"
+                                ref={lastProductElementRef}
+                            >
+                                <Link to={`/products/${product._id}`} className="product-link">
+                                    <div className="product-item">
+                                        <img src={product.imagenes[0]} alt={product.nombre} className="product-image" />
 
-                                    <div className="product-content">
-                                        <span className="product-name">{product.nombre}</span>
+                                        <div className="product-content">
+                                            <span className="product-name">{product.nombre}</span>
 
-                                        <div className="product-actions">
-                                            <button
-                                                className="download-button"
-                                                onClick={(event) => handleDownload(product.imagenes[0], product.nombre, event)}
-                                                aria-label="Descargar imagen"
-                                            >
-                                                <FaDownload />
-                                            </button>
-                                            <button
-                                                className="share-button"
-                                                onClick={(event) => handleShare(product._id, event)}
-                                                aria-label="Compartir producto"
-                                            >
-                                                <FaShareAlt />
-                                            </button>
+                                            <div className="product-actions">
+                                                <button
+                                                    className="download-button"
+                                                    onClick={(event) => handleDownload(product.imagenes[0], product.nombre, event)}
+                                                    aria-label="Descargar imagen"
+                                                >
+                                                    <FaDownload />
+                                                </button>
+                                                <button
+                                                    className="share-button"
+                                                    onClick={(event) => handleShare(product._id, event)}
+                                                    aria-label="Compartir producto"
+                                                >
+                                                    <FaShareAlt />
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </Link>
-                        </div>
-                    ))
-                ) : (
-                    <p>No hay productos disponibles para esta marca.</p>
-                )}
+                                </Link>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div key={product._id} className="product-item-container">
+                                <Link to={`/products/${product._id}`} className="product-link">
+                                    <div className="product-item">
+                                        <img src={product.imagenes[0]} alt={product.nombre} className="product-image" />
+
+                                        <div className="product-content">
+                                            <span className="product-name">{product.nombre}</span>
+
+                                            <div className="product-actions">
+                                                <button
+                                                    className="download-button"
+                                                    onClick={(event) => handleDownload(product.imagenes[0], product.nombre, event)}
+                                                    aria-label="Descargar imagen"
+                                                >
+                                                    <FaDownload />
+                                                </button>
+                                                <button
+                                                    className="share-button"
+                                                    onClick={(event) => handleShare(product._id, event)}
+                                                    aria-label="Compartir producto"
+                                                >
+                                                    <FaShareAlt />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        );
+                    }
+                })}
             </div>
+            {loading && (
+                <div className="loading">
+                    <div className="spinner"></div>
+                </div>
+            )}
+
+            {!hasMore && <div className="end-message">No hay más productos para mostrar.</div>}
         </div>
     );
+
 };
 
 export default BrandProducts;
